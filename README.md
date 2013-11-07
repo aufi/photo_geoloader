a978e4f2a13bc3d53f9bc0fd95ad89fb4c65fa19
Marek Aufart <aufi.cz@gmail.com>
2013-11-07 21:25:13 +0100

    readme update, position attr added

--- README.md
+++ README.md
@@ -3,8 +3,27 @@ photo_geoloader
 
 [![Build Status](https://travis-ci.org/aufi/photo_geoloader.png)](https://travis-ci.org/aufi/photo_geoloader)
 
-Gem loading GPS coordinates from photo's EXIF informations into (Rails) models.
+Ruby gem loading GPS coordinates from photo's EXIF informations into (Rails) models.
 
 Created and tested for photos from iPhone and placing it to model.
 
 **Note: not ready for production use yet**
+
+Usage
+-----
+Simplest - load position data and handle on your own
+```
+loader = PhotoGeoloader.new('path/to/photo.jpg')
+p loader.position[:latitude]
+p loader.position[:longitude]
+p loader.position[:altitude]
+```
+Rails model example (TODO)
+```
+# photo, latitude, longitude, altitude
+class Photo < ActiveRecord::Base
+  after_create do
+    PhotoGeoloader.new(photo.path).place_attributes self
+  end
+end
+```
--- lib/photo_geoloader.rb
+++ lib/photo_geoloader.rb
@@ -1,22 +1,26 @@
 class PhotoGeoloader
   
-  attr_accessor :photo_path, :data, :attributes
+  attr_accessor :photo_path, :data, :attributes, :position
   
   def initialize(photo_path, attributes = [:latitude, :longitude, :altitude])
     self.photo_path = photo_path
     self.attributes = attributes
+    self.position = {}
     load_exif_data
   end
   
   def load_exif_data
     self.data = EXIFR::JPEG.new(photo_path).exif.to_a.first
+    attributes.each do |attr|
+      self.position[attr.to_sym] = data.fields[:gps].fields["gps_#{attr}".to_sym].to_f
+    end
   end
   
   def place_attributes(model)
     success = false
     attributes.each do |attr|
       if model.respond_to? attr
-        model.send "#{attr}=".to_sym, data.fields[:gps].fields["gps_#{attr}".to_sym].to_f
+        model.send "#{attr}=".to_sym, position[attr.to_sym]
         success = true
       end
     end
